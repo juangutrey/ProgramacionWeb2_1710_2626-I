3c70e8a01bd09721b7a09213145a15799c0f7ab8
juangutrey <juangurey@gmail.com>
2025-08-25 19:41:23 -0600

    Tarea de HTML con JS

---
+++ 21082025/app.js
@@ -0,0 +1,19 @@
+function Nombre()
+{
+    alert("Juan Gutierrez")
+}
+
+function Edad()
+{
+    alert("21 años")
+}
+
+function Estatura()
+{
+    alert("172 cm")
+}
+
+function Materia()
+{
+    alert(true)
+}
